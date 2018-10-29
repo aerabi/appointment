784a75e87027cbe58b0325b9cd823fec9e994c8e
Mohammad-Ali AʼRÄBI <aerabi@gmx.de>
2018-10-29 14:13:25 +0100

    Add cross-platform notification

--- ind.py
+++ ind.py
@@ -1,6 +1,6 @@
 import json
+from plyer import notification
 import requests
-import subprocess
 import time
 
 
@@ -10,13 +10,17 @@ def slot_notifier(branch='AM', persons=1, initial_slot=None):
             url = 'https://portal.ind.nl/oap/api/desks/%s/slots/?productKey=DOC&persons=%d' % (branch, persons)
             r = requests.get(url)
             time_slots = json.loads(r.content[5:])
-            soonest_date = time_slots['data'][0]['date']
-            print(r.status_code, soonest_date)
+            soonest_slot = time_slots['data'][0]
+            soonest_date = soonest_slot['date']
+            start_time = soonest_slot['startTime']
+            end_time = soonest_slot['endTime']
+            print(r.status_code, soonest_date, start_time, '-', end_time)
             if initial_slot is None:
                 initial_slot = soonest_date
             if soonest_date < initial_slot:
                 # send desktop notification
-                subprocess.call(['notify-send', 'Time Slot Found', '%s %s' % (branch, soonest_date)])
+                notification_message = '%s %s %s-%s' % (branch, soonest_date, start_time, end_time)
+                notification.notify(title='Time Slot Found', message=notification_message, app_name='Slot Finder')
         except Exception as e:
             print(e)
         time.sleep(30)
